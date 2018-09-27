024aeb0dba2e4605cf30aacf4c9cd30660c78bd3
Andrzej Prałat <apralat@greywizard.com>
2018-09-27 13:34:39 +0200

    Resolve #2 - Implement new fields returned by coins endpoint

--- coin_info.go
+++ coin_info.go
@@ -1,7 +1,11 @@
 package coinpaprika
 
+// CoinInfo stores basic currency information
 type CoinInfo struct {
-	ID     string `json:"id"`
-	Name   string `json:"name"`
-	Symbol string `json:"symbol"`
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Symbol   string `json:"symbol"`
+	Rank     int64  `json:"rank"`
+	IsNew    bool   `json:"is_new"`
+	IsActive bool   `json:"is_active"`
 }
