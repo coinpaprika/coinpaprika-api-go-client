b90a8fc90c39383af330189f056ca142b5714433
Andrzej Prałat <apralat@greywizard.com>
2018-09-28 10:33:34 +0200

    Add example of search

---
+++ examples/search/search.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/coinpaprika/coinpaprika-api-go-client"
+)
+
+func main() {
+	paprikaClient, err := coinpaprika.NewClient()
+	if err != nil {
+		panic(err)
+	}
+
+	// Search currencies and exchanges with "bitcoin" in name.
+	searchOpts := &coinpaprika.SearchOptions{Categories: "currencies,exchanges"}
+	searchResult, err := paprikaClient.Search("bitcoin", searchOpts)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Currencies:")
+	for _, c := range searchResult.Currencies {
+		fmt.Printf("Rank: %-4d Name: %-20s Symbol: %-4s\n", c.Rank, c.Name, c.Symbol)
+	}
+
+	fmt.Println("\nExchanges:")
+	for _, c := range searchResult.Exchanges {
+		fmt.Printf("Rank: %-4d Name: %-20s\n", c.Rank, c.Name)
+	}
+}
